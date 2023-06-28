b3fc266d5225b1a38c1872eb7846ae4d2fa7f8a6
miampf <miampf@proton.me>
2023-06-28 17:09:35 +0200

    particles now play almost correctly

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -9,7 +9,7 @@ public class BallMovement : MonoBehaviour
     // movement vars
     [SerializeField] float speed;
     private Rigidbody rigid;
-    private Vector3 movementDirection = Vector3.zero;
+    private Vector3 inputDirection = Vector3.zero;
 
     // jump vars
     [SerializeField] float jumpSpeed;
@@ -18,40 +18,60 @@ public class BallMovement : MonoBehaviour
     private bool canDoubleJump = false;
 
     // Movement particles
-    private ParticleSystem particleSystem;
+    [SerializeField] GameObject ParticleSystemContainer;
+    private ParticleSystem dustParticles;
+    private bool particlesPlaying = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponent<Rigidbody>();
-        particleSystem = GetComponentInChildren<ParticleSystem>();
+        dustParticles = ParticleSystemContainer.GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        movementDirection = Vector3.zero;
+        ParticleSystemContainer.transform.position = transform.position;
+        inputDirection = Vector3.zero;
 
         // moving
         if (Input.GetKey(KeyCode.W))
         {
-            movementDirection += Vector3.forward;
+            inputDirection += Vector3.forward;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            movementDirection += Vector3.back;
+            inputDirection += Vector3.back;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            movementDirection += Vector3.right;
+            inputDirection += Vector3.right;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            movementDirection += Vector3.left;
+            inputDirection += Vector3.left;
         }
 
-        rigid.AddForce(movementDirection.normalized * speed, ForceMode.Impulse);
+        rigid.AddForce(inputDirection.normalized * speed, ForceMode.Impulse);
+
+        if (grounded && inputDirection != Vector3.zero)
+        {
+            Debug.Log("hi");
+            ParticleSystemContainer.transform.rotation = Quaternion.FromToRotation(dustParticles.shape.position, -inputDirection.normalized);
+
+            if (!particlesPlaying)
+            {
+                dustParticles.Play();
+                particlesPlaying = true;
+            }
+        }
+        else
+        {
+            dustParticles.Stop();
+            particlesPlaying = false;
+        }
 
         // jump + double jump
         if (Input.GetKeyDown(KeyCode.Space) && (grounded | canDoubleJump))
