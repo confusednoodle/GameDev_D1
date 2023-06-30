06508d17bc5b86c822fb1d42e0a4637c19cd326a
confusednoodle <h8pdki@protonmail.com>
2023-06-30 18:40:39 +0200

    worked a bit on level and implemented script for switch & it going down and colour change, this can be reused, the parts to come can't

---
+++ Assets/Scripts/Lvl3RightSwitch.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lvl3RightSwitch : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] Transform switchTrans;
+    [SerializeField] Material colour;
+    private bool pressed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!pressed)
+        {
+            colour.color = new Color(1, 0, 0);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if ((collision.gameObject == player) && !pressed)
+        {
+            pressed = true;
+            switchTrans.position -= new Vector3(0, 0.09f, 0);
+            colour.color = new Color(0, 1, 0);
+        }
+    }
+}
