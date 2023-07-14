a8ee98cd9f21e74b6d4ce317cc2139638ccc979b
miampf <miampf@proton.me>
2023-07-14 14:43:50 +0200

    added particle system to cheese

--- Assets/Scripts/ItemBehaviour.cs
+++ Assets/Scripts/ItemBehaviour.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class ItemBehaviour : MonoBehaviour
 {
+    [SerializeField] ParticleSystem ParticleSystem;
+    [SerializeField] MeshRenderer Renderer;
 
     private void OnTriggerEnter(Collider col)
     {
-        Destroy(gameObject);
+        StartCoroutine(StartDestruction());
+    }
 
+    IEnumerator StartDestruction()
+    {
+        Renderer.enabled = false;
+        ParticleSystem.Play();
+        yield return new WaitForSeconds(1.5f);
+        Destroy(this.gameObject);
     }
 }
